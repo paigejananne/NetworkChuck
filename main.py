8d52b00e4ab1e4b3c8689df01eb9dd59dc5b7a08
Paige Olivo <100541207+paigejananne@users.noreply.github.com>
2023-02-10 23:37:39 +0000

    List, index, negative index

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
 #string
 camping_stuff = "tent, sleeping bags, water, raspberry pi, coffee, knife, ethernet cable, flash drive, beard oil, marshmallows"
+
 #list
+#ordered
+#zero-indexed
+camping_list = ["tent", "sleeping bags", "water", "raspberry pi", "coffee", "knife", "ethernet cable", "flash drive", "beard oil", "marshmallows"]
+
+camp_site = ["Crystal Lake", 404, 89.3, True]
+
+#who brings what?
+me = camping_list[4]
+
+you = camping_list[3]
+
+#negative index
+#access last item in list
+them = camping_list[-1]
+
+print(me)
+
+print(you)
+
+print(them)
+
+print(camping_list)
