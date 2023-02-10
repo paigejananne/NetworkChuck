f11162194e20b0e5689a49313a0c1947d52385cf
Paige Olivo <100541207+paigejananne@users.noreply.github.com>
2023-02-10 23:52:21 +0000

    methods: add 1, add multiple

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ camping_stuff = "tent, sleeping bags, water, raspberry pi, coffee, knife, ethern
 #zero-indexed
 camping_list = ["tent", "sleeping bags", "water", "raspberry pi", "coffee", "knife", "ethernet cable", "flash drive", "beard oil", "marshmallows"]
 
-camp_site = ["Crystal Lake", 404, 89.3, True]
-
 #who brings what?
 me = camping_list[4]
 
@@ -24,3 +22,26 @@ print(you)
 print(them)
 
 print(camping_list)
+
+#methods
+supplies = ["tent", "sleeping bags", "water", "raspberry pi", "coffee", "knife", "ethernet cable", "flash drive", "beard oil", "marshmallows"]
+
+camp_site = ["Crystal Lake", 404, 95.5, False]
+
+#add to list (one at a time) to end of list
+supplies.append("toilet paper")
+
+supplies.append("bidet")
+
+#add to list(multiple pieces of data) to end of list
+supplies.extend(["formula", "diapers"])
+
+supplies = supplies + ["dog food", "treats"]
+
+#add to list to beginning
+supplies.insert(0,"blankets")
+
+#add to list second to last
+supplies.insert(-1, "leashes")
+
+print(supplies)
